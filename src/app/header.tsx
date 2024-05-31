d4c755ee5057d4c5ceb43d7b91fd601e451d1af6
Malith Kuruppu <info.mkappworks@gmail.com>
2024-05-31 22:10:12 +0200

    feat: Add header component for navigation

---
+++ src/app/header.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { ModeToggle } from "@/components/ui/mode-toggle";
+import { SignInButton, UserButton } from "@clerk/nextjs";
+import { Authenticated, Unauthenticated } from "convex/react";
+
+export function Header() {
+  return (
+    <div className="bg-slate-900 py-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-2xl font-bold">Doc Search</div>
+        <div>
+          <Unauthenticated>
+            <SignInButton />
+          </Unauthenticated>
+          <Authenticated>
+            <div className="flex gap-4">
+              <ModeToggle />
+              <UserButton />
+            </div>
+          </Authenticated>
+        </div>
+      </div>
+    </div>
+  );
+}
