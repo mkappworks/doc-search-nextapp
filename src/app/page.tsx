2561762bfe050ce26d66ff0e6fff61943ab495c1
Malith Kuruppu <info.mkappworks@gmail.com>
2024-05-31 20:47:17 +0200

    feat: Update page component with Convex document creation and display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 "use client";
 
 import { SignInButton, UserButton } from "@clerk/nextjs";
-import { Authenticated, Unauthenticated, useQuery } from "convex/react";
+import { api } from "@convex/_generated/api";
+import {
+  Authenticated,
+  Unauthenticated,
+  useMutation,
+  useQuery,
+} from "convex/react";
 
 export default function Home() {
+  const createDocument = useMutation(api.documents.createDocument);
+  const documents = useQuery(api.documents.getDocuments);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Unauthenticated>
@@ -11,6 +20,14 @@ export default function Home() {
       </Unauthenticated>
       <Authenticated>
         <UserButton />
+        <button
+          onClick={() => {
+            createDocument({ title: "hello world" });
+          }}
+        >
+          Click Me
+        </button>
+        {documents?.map((doc) => <div key={doc._id}>{doc.title}</div>)}
       </Authenticated>
     </main>
   );
