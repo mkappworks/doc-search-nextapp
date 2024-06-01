8e6dae6193cb64b7cb6d3097d6d6857b6c25522e
Malith Kuruppu <info.mkappworks@gmail.com>
2024-06-01 21:42:08 +0200

    feat: Update header component to include logo and improve styling

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,25 @@
 import { ModeToggle } from "@/components/ui/mode-toggle";
 import { HeaderActions } from "@/components/header/header-actions";
+import Link from "next/link";
+import Image from "next/image";
 
 export function Header() {
   return (
-    <div className="bg-slate-900 py-4">
+    <div className="z-10 relative dark:bg-slate-900 bg-slate-50 py-4">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-2xl font-bold">Doc Search</div>
+        <div className="flex gap-8 items-center">
+          <Link href="/" className="flex items-center gap-2 text-2xl font-bold">
+            <Image
+              src="/logo.svg"
+              width={40}
+              height={40}
+              className="rounded"
+              alt="search doc icon"
+            />
+            DocSearch
+          </Link>
+        </div>
+
         <div className="flex gap-4 items-center">
           <ModeToggle />
           <HeaderActions />
