d58ce2c774662fb6569a097dcb350eab7f5b857a
Malith Kuruppu <info.mkappworks@gmail.com>
2024-06-02 23:15:35 +0200

    feat: Add @radix-ui/react-toast for displaying toast notifications

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Providers } from "./providers";
 import { cn } from "@/lib/utils";
 import { Header } from "@/components/header/header";
+import { Toaster } from "@/components/ui/toaster";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -31,6 +32,7 @@ export default function RootLayout({
         <Providers>
           <Header />
           {children}
+          <Toaster />
         </Providers>
       </body>
     </html>
