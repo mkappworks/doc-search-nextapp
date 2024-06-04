9b65d8ed44c9c607c4605fb9c1838b08887dc23a
Malith Kuruppu <info.mkappworks@gmail.com>
2024-06-04 16:14:14 +0200

    feat: Add memberships table to Convex schema and mutations for adding and removing users from organizations

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,14 +2,20 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
+  memberships: defineTable({
+    orgId: v.string(),
+    userId: v.string(),
+  }).index("by_orgId_userId", ["orgId", "userId"]),
   docs: defineTable({
     title: v.string(),
     description: v.optional(v.string()),
-    tokenIdentifier: v.string(),
+    tokenIdentifier: v.optional(v.string()),
+    orgId: v.optional(v.string()),
     storageId: v.id("_storage"),
     embedding: v.optional(v.array(v.float64())),
   })
     .index("by_tokenIdentifier", ["tokenIdentifier"])
+    .index("by_orgId", ["orgId"])
     .vectorIndex("by_embedding", {
       vectorField: "embedding",
       dimensions: 1536,
