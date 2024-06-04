ad611d083099a5a4d05b68d9f259de35dead167c
Malith Kuruppu <info.mkappworks@gmail.com>
2024-06-04 16:15:41 +0200

    feat: Update header component to include organization switcher

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,12 +3,13 @@ import Link from "next/link";
 
 import { HeaderActions } from "@/components/header/header-actions";
 import { ModeToggle } from "@/components/ui/mode-toggle";
+import { OrganizationSwitcher } from "@clerk/nextjs";
 
 export function Header() {
   return (
     <div className="relative z-10 bg-zinc-200 py-4 dark:bg-zinc-900">
       <div className="container mx-auto flex items-center justify-between">
-        <div className="flex items-center gap-12">
+        <div className="flex items-center gap-8">
           <Link href="/" className="flex items-center gap-2 text-2xl font-bold">
             <Image
               src="/logo.svg"
@@ -19,7 +20,8 @@ export function Header() {
             />
             DocSearch
           </Link>
-          <nav>
+          <nav className="flex items-center gap-8">
+            <OrganizationSwitcher />
             <Link href="/dashboard" className="hover:text-slate-300">
               Docs
             </Link>
